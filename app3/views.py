5a4d346738f4d55f79735801f47fab1114edbd9c
sanny gupta <sannygupta79@gmail.com>
2024-08-21 10:24:09 +0530

    first commit:

---
+++ app3/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def sann(request):
+    return render(request,'sann.html')
+
+def Gupta(request):
+    return render(request,'Gupta.html')
+
+# Create your views here.
